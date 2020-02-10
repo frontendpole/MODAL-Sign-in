556ffebf8c9ae9d5968f96ff913b2920bbc0411a
Aleksandra <aleksandra.karp1@gmail.com>
2020-02-10 21:18:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Instalacja pakietu odbywa się z użyciem polecenia:
+ ```
+npm install
+```
+
+Aby uruchomić budowanie uruchom w konsoli:
+```
+npm run build
+```
---
+++ src/js/modal.js
@@ -0,0 +1,105 @@
+export class Modal {
+    static ACCEPT = 'ACCEPT';
+    static CANCEL = 'CANCEL';
+
+    constructor(config) {
+        this.config = {
+            hasBackground: true,
+            message: null,
+            htmlContent: null,
+            ...config,
+        };
+        if (this.config.hasBackground) {
+            this.modalBackgroundElement = this.createDiv('modal-background');
+            document.body.appendChild(this.modalBackgroundElement);
+            this.modalBackgroundElement.addEventListener('click', () => {
+                this.cancel();
+            });
+        }
+
+        this.modalElement = this.createDiv('modal');
+        this.modalHeader = this.createDiv('modal-header');
+        this.modalFooter = this.createFooter();
+        this.modalContent = this.createDiv('modal-content');
+
+        if (this.config.message !== null) {
+            this.modalContent.innerHTML = this.config.message;
+        } else if (this.config.htmlContent !== null) {
+            const content = this.config.htmlContent.cloneNode(true);
+            content.style.display = 'block';
+            this.modalContent.appendChild(content);
+            this.config.htmlContent.remove();
+        } else {
+            throw new Error('Modal has no content');
+        }
+        this.modalElement.appendChild(this.modalHeader);
+        this.modalElement.appendChild(this.modalContent);
+        this.modalElement.appendChild(this.modalFooter);
+        document.body.appendChild(this.modalElement);
+    }
+
+    open() {
+        this.modalBackgroundElement.classList.add('open');
+        this.modalElement.classList.add('open');
+        return new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+    }
+
+    _closeModal() {
+        this.modalBackgroundElement.classList.remove('open');
+        this.modalElement.classList.remove('open');
+    }
+
+    close(closeType) {
+        this._closeModal();
+        this.resolve(closeType || Modal.CANCEL);
+    }
+
+    cancel() {
+        this._closeModal();
+        this.resolve(Modal.CANCEL);
+    }
+
+    createDiv(className) {
+        const div = document.createElement('div');
+        div.className = className;
+        return div;
+    }
+
+    createFooter() {
+        const footer = this.createDiv('modal-footer');
+        const accept = document.createElement('button');
+        accept.innerText = 'Accept';
+        accept.classList.add('accept-button');
+        accept.addEventListener('click', () => {
+            const userName = document.getElementById('username-input').value;
+            const password = document.getElementById('password-input').value;
+            if (userName.length >= 5 && userName.match(/[a-zA-Z]/)) {
+                if (password.length >= 8 && password.length <= 15) {
+                    const validPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?=.*[!._-]).{8,15}$/;
+                    if (password.match(validPassword)) {
+                        alert('Welcome to our website. Enjoy!');
+                        this.close(Modal.ACCEPT);
+                    } else {
+                        alert('Your password must contain at least 1 lowercase alphabetical character, 1 uppercase alphabetical character and 1 of these characters: . _ ! -. Please try again.');
+                    }
+                } else {
+                    alert("Password's lentgh should be 8-15 characters. Please try again.");
+                };
+            } else {
+                alert('Username should have at least 5 alphabetical characters. Please try again.');
+            }
+        });
+        const cancel = document.createElement('button');
+        cancel.innerText = 'Close';
+        cancel.classList.add('cancel-button');
+        cancel.addEventListener('click', () => {
+            this.close(Modal.CANCEL);
+        });
+        footer.appendChild(accept);
+        footer.appendChild(cancel);
+        return footer;
+    }
+}
